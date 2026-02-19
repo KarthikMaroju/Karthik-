3339e56df1bde487d230807b47048e78a6afad95
Maroju Karthik <karthikmaroju479@gmail.com>
2026-02-19 20:01:58 +0530

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              bashInteractive # full bash with readline/completion so prompts render correctly
+              buf
+              crane
+              git
+              gnumake
+              less
+              gnused # force Linux `sed` everywhere
+              go_1_24 # must match GO_VERSION in Dockerfile
+              gopls
+              golangci-lint
+              goreleaser
+              nixfmt-rfc-style
+              nodejs_24 # for Pulumi, must match values in package.json
+              openssh
+              pulumi
+              pulumiPackages.pulumi-nodejs
+              google-cloud-sdk
+              vim
+            ];
+            shellHook = ''
+              unset DEVELOPER_DIR # to avoid suprious warning: unhandled Platform key FamilyDisplayName
+              export SHELL=${bashInteractive}/bin/bash
+
+              if [ -t 1 ]; then
+                export PS1="[defang:nix] \w$ "
+              fi
+            '';
+          };
+        packages.defang-cli = pkgs.callPackage ./pkgs/defang/cli.nix { };
+        packages.defang-bin = pkgs.callPackage ./pkgs/defang { };
+      }
+    );
+}
